50c139e7e0b94b77bdc83b417c1c16a2a26d0d1f
mouhcine wildane <muhssenwildane@gmail.com>
2024-07-22 20:24:37 +0000

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    (function() {
+      var task = true; // This task is scoped to the IIFE
+      var task2 = false; // This task2 is scoped to the IIFE
+    })();
   }
 
   return [task, task2];
 }
+
