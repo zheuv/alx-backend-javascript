a4db30d14e1692bc24613bf172285d97881f7461
mouhcine wildane <muhssenwildane@gmail.com>
2024-07-24 20:53:30 +0100

    3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
