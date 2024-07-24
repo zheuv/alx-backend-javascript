caad63c3ecb89eafff25240be4dc79366631ef5c
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-07-24 19:54:29 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve("Response received");
-      } else {
-        reject("Failed to get response");
-      }
-    }, 1000);
-  });
+  return Promise.resolve(true);
 }
