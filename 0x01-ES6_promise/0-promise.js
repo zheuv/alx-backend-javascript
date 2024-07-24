e704098f78daa44b4d048a24b291990448292dc7
mouhcine wildane <muhssenwildane@gmail.com>
2024-07-24 18:42:42 +0000

    0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
 	return new Promise((resolve, reject) => {
-		setTimeout (() {
-			cosnt succes = true;
+		setTimeout (() => {
+			const success = true;
 			if (success) {
 				resolve("success");
 			}	else {
