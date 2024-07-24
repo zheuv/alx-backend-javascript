1d947928b4abb1a183476dc83c7009ec7102965e
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-07-24 23:01:00 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise.allSettled([signUpUser(firstname, lastName), uploadPhoto(fileName)])
+	return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
 	.then((results) => {
 		return results.map((result) => {
 			if (result.status === "fulfilled") {
