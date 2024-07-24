cbeea79a5c05a675a0c2d9102ebf5d7842a2aa4a
mouhcine wildane <muhssenwildane@gmail.com>
2024-07-24 22:52:08 +0100

    6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import { signUpUser } from './4-user-promise.js';
+import { uploadPhoto } from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+	const signUpUser = signUpUser(firstname, lastName);
+	const uploadPhoto = uploadPhoto(fileName);
+
+	return Promise.allSettled([signUpUser, uploadPhoto])
+	.then((results) => {
+		return results.map((result) => {
+			if (result.status === "fulfilled") {
+				return {
+					status: "fulfilled",
+					value: result.value,
+				};
+			} else {
+				return {
+					status: "rejected",
+					value: result.reason,
+				};
+			}
+		});
+	});
+}
