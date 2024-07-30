a3958fce1fc119961a99cc1d211760a224915154
mouhcine wildane <muhssenwildane@gmail.com>
2024-07-30 18:25:38 +0100

    0, 1)

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(students) {
+	if (!Array.isarray(students) || students.length === 0) {
+		return [];
+	}
+	ids = students.map(student => student.id);
+	return ids;
+}
