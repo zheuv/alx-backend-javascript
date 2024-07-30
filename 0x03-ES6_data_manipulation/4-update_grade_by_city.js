a237ab888280a2720a9a9670fbd3c16a0166c715
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-07-30 23:42:40 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
-export default function updateStudentGradeByCity (students, city, newGrades) {
-	return students
-		.filter(student => student.location === city)
-		.map(student => {
-			const gradObj = newGrades.find(grade => grade.studentId === student.id);
-			return {
-				...student,
-				grade: gradeObj ? gradObj = "N/A"
-			};
-		});
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : "N/A"
+      };
+    });
 }
