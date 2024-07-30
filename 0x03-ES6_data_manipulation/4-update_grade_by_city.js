78278e138cbf4df3c6c520dbf8c6554cd1689a1f
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-07-30 23:25:56 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,4 @@
-default export function updateStudentGradeByCity (students, city, newGrades) {
+export default function updateStudentGradeByCity (students, city, newGrades) {
 	return students
 		.filter(student => student.location === city)
 		.map(student => {
