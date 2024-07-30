a9ef4020b1ba354ae23dbaab3fa3fda2bb14aae9
mouhcine wildane <muhssenwildane@gmail.com>
2024-07-30 23:17:17 +0000

    8 correction

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
-        grade: gradeObj ? gradeObj.grade : "N/A"
+        grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
 }
