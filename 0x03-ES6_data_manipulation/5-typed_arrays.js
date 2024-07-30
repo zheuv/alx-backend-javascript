2495dddc966fa5c3ec32f98606c35a4fd3b82041
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-07-30 23:26:10 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-default export function createInt8TypedArray(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
