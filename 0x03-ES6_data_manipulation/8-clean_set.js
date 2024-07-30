d18b12bfe4f5d08c3ddd6f1a8043bef6b776e4f1
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-07-31 00:11:52 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== "string" || startString === ""){
+    return ;
+  }
   return [...set]
     .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
