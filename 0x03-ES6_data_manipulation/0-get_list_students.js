85430cb1a75037022924ba9c3363f99771f80a91
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-07-31 16:02:00 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,14 @@
-export default function getListStudents() {
+export default function getListStudentsss() {
   return [
     { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
     { firstName: 'Serena', id: 3, location: 'San Francisco' },
+  ];
+}
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
