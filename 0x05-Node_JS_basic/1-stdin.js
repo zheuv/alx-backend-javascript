df66c6f61b5408e9226725a90aac748e9348f33f
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-09-18 23:33:48 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
-console.log("Welcome to Holberton School, what is your name?")
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-	const name = data.toString().trim();
-	console.log(`Your name is: ${name}`);
-	process.stdin.end();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-	console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
