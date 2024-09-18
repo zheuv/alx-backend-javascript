10b8401e0c3d3aca4fbecd41bd46b644a2967c53
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-09-18 23:40:31 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,33 +1,33 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n').filter((line) => line.trim().length > 0);
-    const students = lines.slice(1);
-    const studentsCount = students.length;
-    if (studentsCount === 0) {
-        console.log('Number of students: 0');
-        return;
-    }
-    console.log(`Number of students: ${studentsCount}`);
-
-    const fields = {};
-    students.forEach((student) => {
-        const [firstname, , , field] = student.split(",");
-        if (fields[field]) {
-            fields[field].push(firstname);
-        } else {
-            fields[field] = [firstname];
-        }
-    });
-
-    for (const [field, firstnames] of Object.entries(fields)) {
-        console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
-    }
-
-} catch (error) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
+  }
+
+  const data = fs.readFileSync(path, 'utf8');
+  const lines = data.split('\n');
+  const hashtable = {};
+  let students = -1;
+  for (const line of lines) {
+    if (line.trim() !== '') {
+      const columns = line.split(',');
+      const field = columns[3];
+      const firstname = columns[0];
+      if (students >= 0) {
+        if (!Object.hasOwnProperty.call(hashtable, field)) {
+          hashtable[field] = [];
+        }
+        hashtable[field] = [...hashtable[field], firstname];
+      }
+      students += 1;
+    }
+  }
+  console.log(`Number of students: ${students}`);
+  for (const key in hashtable) {
+    if (Object.hasOwnProperty.call(hashtable, key)) {
+      console.log(`Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}`);
+    }
   }
 }
 
