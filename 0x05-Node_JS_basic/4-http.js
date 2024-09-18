0c316741f955917e39163b40c78563ab6a4ff906
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-09-18 23:42:25 +0100

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello Holberton School!');
-});
-
-app.listen(1245, ()=> {
-	console.log('Server is running on port 1245');
-});
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
 
 module.exports = app;
